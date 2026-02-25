07d6222524fe90c35470ee88259af17aec1aa68f
Hernan Gonzalez <hernan.gonzalez.magliano@gmail.com>
2026-02-24 22:29:28 -0300

    - Added next-theme library

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,3 @@
+export default function SignInPage() {
+  return <div className="w-full bg-black">SignInPage</div>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import { ComponentExample } from "@/components/component-example";
-
 export default function Page() {
-return <ComponentExample />;
+  return (
+    <main className="flex h-screen items-center justify-center bg-black">
+      <h1 className="text-white font-bold text-4xl">
+        Bienvenido a TurnoVita !
+      </h1>
+    </main>
+  );
 }
